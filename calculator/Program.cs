3f76ea58140f4d1599e25b0ee890d20790df288d
nizovcilya <nizovcilya@gmail.com>
2018-04-01 13:12:09 +0500

    create repository

---
+++ calculator/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Sprache;
+
+namespace calculator
+{
+    internal static class Program
+    {
+        public static void Main()
+        {
+            while (Prompt(out var line))
+            {
+                try
+                {
+                    var parsed = ExpressionParser.ParseExpression(line);
+                    Console.WriteLine($"Parsed as {parsed}");
+                    Console.WriteLine($"Value is {parsed.Compile()()}");
+                }
+                catch (ParseException ex)
+                {
+                    Console.WriteLine($"There was a problem with your input: {ex.Message}");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        private static bool Prompt(out string value)
+        {
+            Console.Write("Enter a numeric expression, or 'q' to quit: ");
+            var line = Console.ReadLine();
+            
+            if (line?.ToLowerInvariant().Trim() == "q")
+            {
+                value = null;
+                return false;
+            }
+
+            value = line;
+            return true;
+        }
+    }
+}
